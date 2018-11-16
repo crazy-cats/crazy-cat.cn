377e97857377ed63bfb201c642f7801ee811076d
Bruce <bruce.z@infinitytesting.com.au>
2018-11-16 21:52:37 +0800

    ^ build CrazyCat instance

---
+++ README.md
@@ -0,0 +1,9 @@
+# CrazyCat
+This is a simple PHP framework for creating website or internal data management system.
+
+# How to install
+This framework is based on composer. 
+Download the files and use below commands to create a project:
+```
+composer create-project
+```
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * Copyright © 2018 CrazyCat, Inc. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+/**
+ * @category CrazyCat
+ * @package CrazyCat\Framework
+ * @author Bruce Z <152416319@qq.com>
+ * @link http://crazy-cat.co
+ */
+error_reporting( E_ALL | E_STRICT );
+
+/**
+ * global static variables
+ */
+require 'definitions';
+
+/* @var $composerLoader \Composer\Autoload\ClassLoader */
+$composerLoader = require 'vendor/autoload.php';
+
+\CrazyCat\Framework\App::getInstance()->run( $composerLoader );
