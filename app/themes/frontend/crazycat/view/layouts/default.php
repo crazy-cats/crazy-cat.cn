c21cb9d4348eb53b75f9f068ba89bbef264555ad
zengliwei <zengliwei@163.com>
2020-06-22 11:48:40 +0800

    + add front-end template

---
+++ app/themes/frontend/crazycat/view/layouts/default.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * Copyright © 2020 CrazyCat, Inc. All rights reserved.
+ * See COPYRIGHT.txt for license details.
+ */
+
+/**
+ * @category CrazyCat
+ * @package  CrazyCat\Theme\Frontend\CrazyCat
+ * @author   Liwei Zeng <zengliwei@163.com>
+ * @link     https://crazy-cat.cn
+ */
+return [
+    'template' => '1column',
+    'css'      => [
+        'css/styles.css'
+    ],
+    'blocks'   => [
+        'head'     => [
+            'js-in-head' => ['class' => 'CrazyCat\Base\Block\Head']
+        ],
+        'messages' => [
+            'message-box' => ['class' => 'CrazyCat\Base\Block\Message']
+        ],
+        'footer'   => [
+            'footer-content' => ['class' => 'CrazyCat\Base\Block\Template']
+        ]
+    ]
+];
---
+++ app/themes/frontend/crazycat/view/templates/pages/2columns_left.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * Copyright © 2020 CrazyCat, Inc. All rights reserved.
+ * See COPYRIGHT.txt for license details.
+ */
+
+/* @var $this \CrazyCat\Framework\App\Component\Theme\Page */
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?= getLangCode(); ?>" lang="<?= getLangCode(); ?>">
+<head>
+    <title><?= htmlEscape($this->getPageTitle()); ?></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="keywords" value="<?= htmlEscape($this->getData('meta_keywords')); ?>"/>
+    <meta name="description" value="<?= htmlEscape($this->getData('meta_description')); ?>"/>
+    <meta name="robots" value="<?= htmlEscape($this->getData('meta_robots')); ?>"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+    <link rel="shortcut icon" href="<?= $this->getThemeUrl('images/favicon.ico'); ?>" type="image/x-icon"/>
+    <?= $this->getCssScripts(); ?>
+    <?= $this->getSectionHtml('head'); ?>
+</head>
+<body class="two-columns <?= $this->getLayoutName('-') ?>">
+<div class="header-wrapper wrapper">
+    <div class="header">
+        <?= $this->getSectionHtml('header'); ?>
+    </div>
+</div>
+<div class="main-wrapper wrapper">
+    <div class="left">
+        <?= $this->getSectionHtml('left'); ?>
+    </div>
+    <div class="main">
+        <?= $this->getSectionHtml('messages'); ?>
+        <?= $this->getSectionHtml('main'); ?>
+    </div>
+</div>
+<div class="footer-wrapper wrapper">
+    <div class="footer">
+        <?= $this->getSectionHtml('footer'); ?>
+    </div>
+</div>
+</body>
+</html>
